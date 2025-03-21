5cdd89645c3a8acfff805b970d239947df1202c0
Ayman <aimanelkilani@gmail.com>
2025-03-21 02:27:37 +0200

    Part 03 - Department module and DbContext

---
+++ MVC_Project.DataAccess/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+namespace MVC_Project.DataAccess.Data.Contexts
+{
+    class ApplicationDbContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("connection");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
