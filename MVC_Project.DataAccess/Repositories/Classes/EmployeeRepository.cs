521b18d6be45613e3076539e898b0515e0a15302
Ayman <aimanelkilani@gmail.com>
2025-07-07 10:09:50 +0300

    Resolved merge conflict: kept EmployeeRepository.cs

---
+++ MVC_Project.DataAccess/Repositories/Classes/EmployeeRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC_Project.DataAccess.Data.Contexts;
+using MVC_Project.DataAccess.Models.DepartmentModel;
+using MVC_Project.DataAccess.Repositories.Interfaces;
+
+namespace MVC_Project.DataAccess.Repositories.Classes
+{
+    public class EmployeeRepository(ApplicationDbContext dbContext) : GenericRepository<Employee>(dbContext), IEmployeeRepository
+    {
+        int IEmployeeRepository.Add(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
